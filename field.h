67ddc87e077ba5738128f858373e3bb72593a76d
Sasha <l.alexiosan@gmail.com>
2023-04-27 16:31:56 +0300

    First Commit

---
+++ field.h
@@ -0,0 +1,228 @@
+#pragma once
+#include <vector>
+#include <iostream>
+
+class Tile {
+    int x = 0, y = 0, status = 0, color = 0; // status = 0 - пустая клетка
+                                  // status = 1 - статичная клетка
+                                  // status = 2 - движение
+    char image = '@';
+public:
+    Tile() {};
+    Tile(int x, int y, char i = '@', int s = 0, int c = 0) : x(x), y(y), image(i), status(s), color(c) {};
+
+    Tile& operator=(const Tile& a) {
+        if (&a == this) return *this;
+        x = a.x;
+        y = a.y;
+        color = a.color;
+        status = a.status;
+        image = a.image;
+        return *this;
+    }
+
+    void fall() {
+        y++;
+    }
+    void move(int dx) {
+        x += dx;
+    }
+
+    int& getX() {
+        return x;
+    }
+    int& getY() {
+        return y;
+    }
+    char& getI() {
+        return image;
+    }
+    void stop() {
+        status = 1;
+    }
+    int& getStat() {
+        return status;
+    }
+    int getColor() {
+        return color;
+    }
+    void print() {
+        std::cout << x << " " << y << std::endl;
+    }
+
+};
+
+int shapes[7][4] = {
+        1, 3, 5, 7, // I
+        2, 4, 5, 7, // Z
+        3, 5, 4, 6, // S
+        3, 5, 4, 7, // T
+        2, 3, 5, 7, // L
+        3, 5, 7, 6, // J
+        2, 3, 4, 5  // O
+};
+
+class Field
+{
+private:
+    int width, height;
+    std::vector <std::vector<Tile>> field;
+public:
+    Field(int w, int h) : width(w), height(h)
+    {
+        field.resize(height);
+        for (int i = 0; i < height; i++)
+        {
+            std::vector<Tile> v(width);
+            for (int j = 0; j < width; j++)
+            {
+                v[j].getX() = i;
+                v[j].getY() = j;
+                v[j].getI() = '@';
+                v[j].getStat() = 0;
+            }
+
+            field[i] = v;
+        }
+    };
+
+    std::vector<Tile>& operator[](int i) { return field[i]; }
+
+    int get_width() {
+        return width;
+    }
+    int get_height() {
+        return height;
+    }
+
+    void printField()
+    {
+        for (int i = 0; i < field.size(); i++)
+        {
+            for (int j = 0; j < field[i].size(); j++)
+                std::cout << field[i][j].getStat() << " ";
+            std::cout << std::endl;
+        }
+    }
+};
+
+class Figure {
+protected:
+    int type, color; // 1 -
+    std::vector<Tile> tiles;
+public:
+    Figure() {
+        type = rand() % 7;
+        color = 1 + type;
+        for (int i = 0; i < 4; i++)
+        {
+            Tile newTile(shapes[type][i] / 2, shapes[type][i] % 2, '#', 2, color);
+            tiles.push_back(newTile);
+        }
+    };
+    Figure& operator=(const Figure& a) {
+        if (&a == this) return *this;
+        type = a.type;
+        tiles.reserve(a.tiles.size());
+        for (int i = 0; i < a.tiles.size(); i++)
+            tiles[i] = a.tiles[i];
+        return *this;
+    }
+
+    void tofield(Field &field) {
+        for (int i = 0; i < tiles.size(); i++) {
+            field[tiles[i].getY()][tiles[i].getX()] = tiles[i];
+        }
+    }
+
+    void fromfield(Field &field) {
+        for (int i = 0; i < tiles.size(); i++) {
+            field[tiles[i].getY()][tiles[i].getX()] = Tile(tiles[i].getX(), tiles[i].getY(), '@', 0);
+        }
+    }
+
+    void movement(int dx) {
+        for (int i = 0; i < tiles.size(); ++i) {
+            tiles[i].move(dx);
+        }
+    }
+
+    void stop() {
+        for (int i = 0; i < tiles.size(); ++i) {
+            tiles[i].stop();
+        }
+    }
+
+    void rotate() {
+        if (type != 6) {
+            // Center of the rotation.
+            Tile point = tiles[1];
+
+            for (int i = 0; i < tiles.size(); i++) {
+                int x = tiles[i].getY() - point.getY();
+                int y = tiles[i].getX() - point.getX();
+                tiles[i].getX() = point.getX() - x;
+                tiles[i].getY() = point.getY() + y;
+            }
+        }
+    }
+
+    void print() {
+        std::vector<std::vector<Tile>> pole;
+        for (int i = 0; i < 4; i++) {
+            std::vector<Tile> v;
+            for (int j = 0; j < 4; ++j) {
+                v.push_back(Tile(i, j, '0', 0));
+            }
+//            v.resize(4, 0);
+            pole.push_back(v);
+        }
+        for (int i = 0; i < tiles.size(); i++) {
+            pole[tiles[i].getY()][tiles[i].getX()] = tiles[i];
+            std::cout << tiles[i].getStat() << " " << pole[tiles[i].getY()][tiles[i].getX()].getStat() << " ";
+        }
+        std::cout << "\n";
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                std::cout << pole[i][j].getStat() << " ";
+            }
+            std::cout << std::endl;
+        }
+    }
+    bool check(Field field, int dx = 0) {
+        for (int i = 0; i < tiles.size(); i++) {
+            // Check for the shape being in the playfield.
+            if (tiles[i].getX() < 0 || tiles[i].getX() >= field.get_width())
+                return false;
+
+            // The more I look at it, the more I want to puke.
+            if (tiles[i].getY() == field.get_height()-1) {
+//                std::cout << "zamena stat dno" << " ";
+//                 проверка на stop
+//                for (int j = 0; j < tiles.size(); ++j) {
+//                    std::cout << "Stat " << j << ": "<< tiles[i].getStat() << " Stat in field: " << field[tiles[i].getY()][tiles[i].getX()].getStat();
+//                }
+//                std::cout << std::endl;
+                return false;
+            }
+            if (field[tiles[i].getY()][tiles[i].getX()].getStat() == 1) {
+//                std::cout << "zamena stat nyj" << " ";
+                return false;
+            }
+
+//            if (field[tiles[i].getY()][tiles[i].getX()+dx].getStat() == 1) {
+//                std::cout << "zamena stat bok" << " ";
+//                return false;
+//            }
+        }
+
+        return true;
+    }
+    void fall() {
+        for (int i = 0; i < tiles.size(); ++i) {
+            tiles[i].fall();
+        }
+    }
+};
+
---
+++ main.cpp
@@ -0,0 +1,265 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <vector>
+#include "time.h"
+#include "field.h"
+// https://gitlab.com/Zedespook/sfml-tetris/-/blob/master/src/main.cpp
+using namespace sf;
+
+
+
+
+// Main function for running the game.
+int main()
+{
+
+    int width = 10, height = 20;
+    Field field(width, height);
+    Field field1(width, height);
+    Figure a, b;
+    b = a;
+    // Generates a random seed for the session.
+    srand(time(0));
+
+    // The main render for the window.
+    RenderWindow window(VideoMode(320, 480), "Tetris Clone");
+
+    // This is the tile for the tetris block.
+    RectangleShape shape(Vector2f(18.f, 18.f));
+    shape.setFillColor(Color::Blue);
+
+
+    // Shape modifiers.
+    int dx = 0;
+    bool rotate = false;
+    int color = 1;
+
+    int padding = 50;
+
+
+    // Game clock variables.
+    float timer = 0.f, delay = 0.3f;
+    static Clock clock;
+
+    int score = 0.f;
+
+    static Font font;
+    if (!font.loadFromFile("../font.ttf"))
+//        std::cout << "Error loading font file." << std::endl;
+        int a;
+    static Text scoreTextLabel;
+    scoreTextLabel.setFont(font);
+    scoreTextLabel.setString("Cleared:");
+    scoreTextLabel.setCharacterSize(20);
+    scoreTextLabel.setFillColor(Color::White);
+
+    static Text scoreLabel;
+    scoreLabel.setFont(font);
+    scoreLabel.setCharacterSize(24);
+    scoreLabel.setFillColor(Color::White);
+
+    bool FINAL = false;
+
+    int aboba = 0;
+
+    while (window.isOpen()) {
+        float time = clock.getElapsedTime().asSeconds();
+        clock.restart();
+        timer += time;
+
+        Event event;
+        while (window.pollEvent(event)) {
+            switch (event.type) {
+                case Event::Closed:
+                    window.close();
+                    break;
+
+                case Event::KeyPressed:
+
+                    switch (event.key.code) {
+                        case Keyboard::Up:
+                            rotate = true;
+                            break;
+                        case Keyboard::Left:
+                            dx = -1;
+                            break;
+                        case Keyboard::Right:
+                            dx = 1;
+                            break;
+
+                        default:
+                            break;
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        if (Keyboard::isKeyPressed(Keyboard::Down))
+            delay = 0.05;
+
+        if (!FINAL) {
+            // * Movement
+            b = a;
+
+            a.movement(dx);
+            if (!a.check(field)) {
+                a = b;
+            } else {
+                b.fromfield(field);
+                a.tofield(field);
+            }
+
+            b = a;
+            // * Rotation
+            if (rotate) a.rotate();
+
+            if (!a.check(field)) {
+                a = b;
+            } else {
+                b.fromfield(field);
+                a.tofield(field);
+            }
+
+            b = a;
+            // * Tick system (falling)
+            if (timer > delay) {
+                a.fall();
+
+                if (!a.check(field)) {
+                    b.stop();
+                    b.tofield(field);
+                    for (int i = 0; i < width; ++i) {
+                        if (field[0][i].getStat() == 1) {
+//                            std::cout << "stop";
+                            FINAL = true;
+                        }
+                    }
+                    Figure c;
+                    a = c;
+                    a.tofield(field);
+                } else {
+                    b.fromfield(field);
+                    a.tofield(field);
+                }
+
+                timer = 0;
+            }
+            b = a;
+
+            // * Line clear
+            int k = height - 1;
+
+            for (int i = height - 1; i > 0; i--) {
+                int count = 0;
+
+                for (int j = 0; j < width; j++) {
+                    if (field[i][j].getStat() == 1)
+                        count++;
+
+                    field[k][j] = field[i][j];
+                }
+
+                if (count < width)
+                    k--;
+            }
+
+            //         * Scores
+            score += k;
+            scoreLabel.setString(std::to_string(score));
+
+            // * Draw to window
+            window.clear(Color(0x685850FF));
+
+            for (int i = 0; i < height; i++) {
+                for (int j = 0; j < width; j++) {
+                    if (field[i][j].getI() == '#') {
+                        if (field[i][j].getColor() == 1) {
+                            shape.setFillColor(Color(0xCCA8D8FF));
+                        }
+                        else if (field[i][j].getColor() == 2) {
+                            shape.setFillColor(Color(0xF886A8FF));
+                        }
+                        else if (field[i][j].getColor() == 3) {
+                            shape.setFillColor(Color(0xFDC453FF));
+                        }
+                        else if (field[i][j].getColor() == 4) {
+                            shape.setFillColor(Color(0x9ADBC5FF));
+                        }
+                        else if (field[i][j].getColor() == 5) {
+                            shape.setFillColor(Color(0xA0DDE0FF));
+                        }
+                        else if (field[i][j].getColor() == 6) {
+                            shape.setFillColor(Color(0xFE8D6FFF));
+                        }
+                        else if (field[i][j].getColor() == 7) {
+                            shape.setFillColor(Color(0xFCC1B1FF));
+                        }
+                        shape.setPosition(j * 18+padding*1.5, i * 18+padding*1.5);
+                        window.draw(shape);
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < width; ++i) {
+            RectangleShape line(sf::Vector2f((height-1)*18+5, 2));
+            line.move(padding*1.5+18*i, padding*1.5);
+            line.setFillColor(Color(0x12120D44));
+            line.rotate(90);
+
+            window.draw(line);
+        }
+        for (int i = 0; i < height; ++i) {
+            RectangleShape line(sf::Vector2f(width*18, 2));
+            line.move(padding*1.5, padding*1.5+i*18);
+            line.setFillColor(Color(0x12120D44));
+
+            window.draw(line);
+        }
+
+//        RectangleShape line(sf::Vector2f(padding*1.5, 5));
+        RectangleShape line1(sf::Vector2f((height-1)*18+5, 5));
+        line1.move(padding*1.5, padding*1.5);
+
+        RectangleShape line2(sf::Vector2f(width*18, 5));
+        line2.move(padding*1.5, padding*1.5);
+
+        RectangleShape line3(sf::Vector2f((height-1)*18+5, 5));
+        line3.move(padding*1.5+(width*18)+5, padding*1.5);
+
+        RectangleShape line4(sf::Vector2f(width*18, 5));
+        line4.move(padding*1.5, (height-1)*18+padding*1.5);
+
+        line1.setFillColor(Color(0x766092FF));
+        line2.setFillColor(Color(0x766092FF));
+        line3.setFillColor(Color(0x766092FF));
+        line4.setFillColor(Color(0x766092FF));
+
+        line1.rotate(90);
+        line3.rotate(90);
+
+//        line.rotate(45);
+        window.draw(line1);
+        window.draw(line2);
+        window.draw(line3);
+        window.draw(line4);
+
+        dx = 0;
+        rotate = false;
+        delay = 0.3f;
+
+        aboba++;
+        scoreTextLabel.setPosition(window.getSize().x / 2 - 75, 240-padding*2);
+        window.draw(scoreTextLabel);
+
+        scoreLabel.setPosition(window.getSize().x / 2 - 15, 270-padding*2);
+        window.draw(scoreLabel);
+        window.display();
+    }
+//    field.printField();
+//    std::cout << "SCORE: "<< score << " points";
+    return 0;
+}
